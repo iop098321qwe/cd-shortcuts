b790aea549a5884459cef91fd8523772ac9da0e7
Dallas Elliott <iop098321qwe@pm.me>
2025-11-26 17:11:36 -0700

    initial commit

---
+++ cbc-module.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+################################################################################
+# CD SHORTCUTS
+################################################################################
+
+alias back='cd ..'
+alias cdgh='cd ~/Documents/github_repositories'
+alias docs='cd ~/Documents'
+alias downloads='cd ~/Downloads'
+alias home='cd ~'
+alias temp='cd ~/Documents/Temporary'
+alias vault='cd ~/Documents/grymms_grimoires'
